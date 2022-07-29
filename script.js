adc438fae6b9b8be19de3774b2c6658cb6029a6f
Thony <bellotony229@gmail.com>
2022-07-29 10:46:55 +0100

    Add index.html

--- script.js
+++ script.js
@@ -1,73 +1,36 @@
-let computerPlay = () => {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
-  if (randomNumber === 1) {
-    return "rock";
-  } else if (randomNumber === 2) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-};
-// console.log(computerPlay());
-
-//player input
-
-let player = () => {
-  let input = prompt("Rock, Paper or Scissors");
-  let lower = input.toLowerCase();
-  if (lower === "rock") {
-    return "rock";
-  } else if (lower === "paper") {
-    return "paper";
-  } else if (lower === "scissors") {
-    return "scissors";
-  } else {
-    return `${input} is an error`;
-  }
+let getComputerChoice = () => {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
 };
 
-// console.log(player());
-
-let playerSelection = player();
-let computerSelection = computerPlay();
-
-let playRound = () => {
-  if (
-    (playerSelection === "rock" && computerSelection === "rock") ||
-    (playerSelection === "paper" && computerSelection === "paper") ||
-    (playerSelection === "scissors" && computerSelection === "scissors")
-  ) {
-    return `It is a draw ${playerSelection} can not beat ${computerSelection}!`;
-  }
-
-  //player wins
-  else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    return `Player wins ${playerSelection} beats ${computerSelection}!!`;
-  }
-  //computer wins
-  else if (
-    (computerSelection === "rock" && playerSelection === "scissors") ||
-    (computerSelection === "rock" && playerSelection === "paper") ||
-    (computerSelection === "scissors" && playerSelection === "paper")
-  ) {
-    return `Computer wins ${computerSelection} beats ${playerSelection}!!`;
-  }
-
-  else {
-    return `${playerSelection}!!`;
-  }
+let game = (userChoice) => {
+  const computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  console.log(`user  ${userChoice}`);
 };
-let play = playRound();
-
-let game = () => {
-
-  for (let i = 0; i < 5; i++){
- console.log (play);
-}
-}
 game();
+let main = () => {
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
+
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
+
+  scissors_div.addEventListener("click", function () {
+    game("s");
+  });
+};
+main();
